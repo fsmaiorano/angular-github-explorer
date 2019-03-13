af868f86a819f21de41ee4ed6782fffc49c7aa39
Fábio <fsmaiorano@gmail.com>
2019-03-13 17:01:18 -0300

    rafactory and add material

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatButtonModule, MatCheckboxModule } from "@angular/material";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+  exports: [MatButtonModule, MatCheckboxModule]
+})
+export class SharedModule {}
