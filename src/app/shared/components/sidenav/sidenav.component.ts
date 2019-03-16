89de2b8e3112ff5aeb33775de99dd48128c68c3a
Fábio <fsmaiorano@gmail.com>
2019-03-16 16:47:29 -0300

    add changeuser function

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { GithubSingletonService } from '../../services/github/github-singleton.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -12,5 +14,15 @@ export class SidenavComponent {
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(map(result => result.matches));
-  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private githubSingletonService: GithubSingletonService,
+    private router: Router
+  ) {}
+
+  private changeUser(): void {
+    this.githubSingletonService.clearUser();
+    this.router.navigateByUrl('');
+  }
 }
